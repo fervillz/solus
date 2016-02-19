670c51ba0e4e4b55ac2c23d4a64fd9448e2a97b8
Fernando <fervillz@gmail.com>
2016-02-18 16:28:21 -0800

    remove unwanted argument in 404 page

--- 404.php
+++ 404.php
@@ -44,7 +44,7 @@ get_header(); ?>
 					<?php
 						/* translators: %1$s: smiley */
 						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'solus' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+						the_widget( 'WP_Widget_Archives', 'dropdown=1');
 					?>
 
 					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
