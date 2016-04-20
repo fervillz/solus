83002054a704605c51f86bd17ff03bfa4ae571b4
kamena <kamena.dacheva@gmail.com>
2016-04-20 18:02:17 +0300

    Open links from footer in new tabs

--- footer.php
+++ footer.php
@@ -19,9 +19,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'solus' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'solus' ), 'WordPress' ); ?></a>
+			<a target="_blank" href="<?php echo esc_url( __( 'https://wordpress.org/', 'solus' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'solus' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'solus' ), 'solus', '<a href="http://devrix.com" rel="designer">Devrix</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'solus' ), 'solus', '<a target="_blank" href="http://devrix.com" rel="designer">Devrix</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
