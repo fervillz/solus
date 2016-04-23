d9e0f7c1b58d067be9ff042c8fde6369f2b016e8
fernando villamor jr <fervillz@gmail.com>
2016-04-22 18:01:21 -0700

    change homepage content to excerpt

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,29 +8,29 @@
  */
 
 ?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
 	<div class="post_thumb">
 		<?php if (has_post_thumbnail()){ the_post_thumbnail(); } ?>
 	</div>
-
 	<div class="content-wrapper">
-
 		<header class="entry-header">
-			
 			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		</header><!-- .entry-header -->
+		</header>
+		<!-- .entry-header -->
 		<div class="entry-content">
-
-			<?php  the_content('<span class="readmore_bk">Read more...</span>'); ?>
-
-		</div><!-- .entry-content -->
+			<?php  
+				if ( is_home() ) {
+					the_excerpt();
+				}else {
+					the_content();
+				}
+			 ?>
+		</div>
+		<!-- .entry-content -->
 		<footer class="home-entry-footer">
 			<?php solus_home_footer(); ?>
-		</footer><!-- .entry-footer -->
-
+		</footer>
+		<!-- .entry-footer -->
 	</div>
-
-</article><!-- #post-## -->
+</article>
+	<!-- #post-## -->
